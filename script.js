65d9966ae00ca7092274da351dc844d375e8b702
Matt <mpmekita@gmail.com>
2022-06-17 19:36:39 -0400

    update operation functions

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
+//get elements: display, buttons, display text
 const display = document.querySelector('.display');
 
 let buttons = Array.from(document.querySelectorAll('button'));
 
+let problem = display.innerText
+
+//function to display numbers/operators on 'click'
 buttons.map(button => {
     button.addEventListener('click', (e) => {
         switch(e.target.innerText){
             case 'AC': 
                 display.innerText = '';
                 break;
-            case 'Backspace':
-                if(display.innerTxt){
+            case 'Delete':
+                if(display.innerText){
                 display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
+            case '=':
+                operate()
             default:
                 display.innerText += e.target.innerText;
         };
     })
 })
+
+//function to run calculations
 
 
 const operations = {
@@ -38,11 +46,4 @@ function operate(list, operator) {
     return list.reduce(operations[operator]);
 }
 
-let clickedOn = function(){
-
-}
-
-let operands = function operand(){
-    return clickedOn;
-}
-
+console.log(operate([3, 4, 6, 8],'*'))
